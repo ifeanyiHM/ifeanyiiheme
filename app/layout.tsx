6665d78b0f23574242cee98cc935ba7df990a7d4
ifeanyihm <iheme.ifeanyi@yahoo.com>
2024-08-19 08:27:31 +0100

    my portfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "@/app/styles/globals.css";
+import "./Styles/globals.css";
 import { PortfolioProvider } from "./_context/PortfolioContext";
 import Navigation from "./Components/Navigation";
 import MenuPage from "./Components/MenuPage";
